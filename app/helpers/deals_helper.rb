e21d080ad177e2b600eb639e497807a807797107
Greg Lazarev <russianbandit@gmail.com>
2011-10-25 20:20:55 -0700

    displaying time as in units passed

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,5 +1,5 @@
 module DealsHelper
-  def submitted_ago(deal)
-    deal.created_at.strftime("%I:%M %p on %B %d, %Y")
+  def submitted_by_text(deal)
+    "Plugged by: #{deal.user.username} #{time_ago_in_words(deal.created_at)} ago"
   end
 end
